bc9b94ec7cd8250f0090acca87f70857badaced6
Koul-wen-dey <a2722764538@gmail.com>
2022-12-21 21:45:06 +0800

    .

--- hw2/mydataset.py
+++ hw2/mydataset.py
@@ -16,37 +16,66 @@ class Image_dataset(Dataset):
         self.img_path = glob(os.path.join(data_path,'image/*.png'))
         self.label_path = glob(os.path.join(data_path,'label/*.json'))
         self.mask_path = glob(os.path.join(data_path,'mask/*.png'))
-    
+        self.hash = {'background':0,'powder_uncover':1,'powder_uneven':2,'scratch':3}
+        self.normalize = T.Normalize((0.5557),(0.2051))
+
+
     def __getitem__(self, index):
-        # print(self.img_path[index])
         img = read_image(self.img_path[index],ImageReadMode.GRAY)
         mask = read_image(self.mask_path[index],ImageReadMode.GRAY)
         img = F.convert_image_dtype(img,dtype=torch.float)
-        mask = F.convert_image_dtype(mask,dtype=torch.float)
+        mask = F.convert_image_dtype(mask,dtype=torch.uint8)
+        num_ids = len(torch.unique(mask)[1:])
         with open(self.label_path[index]) as file:
-            labels = json.load(file)
+            table = json.load(file)
 
         boxes = []
-        for label in labels['shapes']:
+        label_t = []
+        for label in table['shapes']:
+            x = [label['points'][0][0],label['points'][1][0]]
+            y = [label['points'][0][1],label['points'][1][1]]
             boxes.append([
-                label['points'][0][0],
-                label['points'][0][1],
-                label['points'][1][0],
-                label['points'][1][1]])
+                min(x),
+                min(y),
+                max(x),
+                max(y)
+            ])
+            label_t.append(self.hash[label['label']])
         boxes = torch.as_tensor(boxes,dtype=torch.float)
-        # print(boxes)
-        target = {'masks':mask,'boxes':boxes,'labels':torch.ones((3,),dtype=torch.int8)}
+        
+        mask_list = []
+        for b in boxes:
+            tmp = torch.zeros(mask.shape[1:])
+            x1, y1 = int(torch.round(b[0]).item()), int(torch.round(b[1]).item())
+            x2, y2 = int(torch.round(b[2]).item()), int(torch.round(b[3]).item())
+            tmp[y1:y2, x1:x2] = 1
+            mask_list.append(torch.where(tmp > 0, mask[0], tmp))
+        mask = torch.tensor([m.tolist() for m in mask_list])
+
+        past_len = mask.shape[-1]
+        target = {
+            'masks':mask,
+            'boxes':boxes,
+            'labels':torch.tensor(label_t,dtype=torch.int64),
+            'area':(boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]),
+            'image_id':torch.tensor([index]),
+            'iscrowd':torch.zeros((num_ids,),dtype=torch.int64)
+        }
+        img = self.normalize(img)
         if self.transform is not None:
             img = self.transform(img)
-            # target = self.transform(target)
+            target['masks'] = self.transform(target['masks'])
+            target['boxes'] = target['boxes'] / past_len * img.shape[-1] 
+            target['area'] = target['area'] / past_len**2 * img.shape[-1]**2
 
         return img, target
     
     def __len__(self):
         return len(self.img_path)
 
+
 if __name__ == '__main__':
     path = './class_data/Train/*/'
     trans = T.Compose([T.Resize((256,256))])
     a = Image_dataset(path,trans)
-    print(a[0][1])
+    print(a[1][1])
--- hw2/train.py
+++ hw2/train.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
@@ -7,35 +7,43 @@ from mydataset import Image_dataset
 import torchvision.transforms as T
 from tqdm import tqdm
 from myloss import IoULoss
+from sklearn.model_selection import KFold
 
+def my_collate_fn(batch):
+    return tuple(zip(*batch))
 
-def training(model, dataloader, epoch, loss, optimizer, device):
+def training(model, dataloader, loss, optimizer, device, idx,e):
+
     model.train()
-    for i in range(epoch):
-        with tqdm(dataloader, unit='batch') as data:
-            correct = 0
-            for img, target in data:
-                data.set_description(f'Epoch{i+1}')
-                img, target = img.to(device), target.to(device)
-                optimizer.zero_grad()
-                output = model(img,target)
-                error = loss(output,)
-                error.backward()
-                optimizer.step()
-            torch.save({'model_state_dict':model.state_dict()},'./models/mask_rcnn_adam.pt')
-            print('save checkpoint')
+    with tqdm(dataloader, unit='batch') as data:
+        for img, targets in data:
+            data.set_description(f'Epoch{e+1}')
+            img = [i.to(device) for i in img]
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            output = model(img,targets)
+            losses = sum(loss for loss in output.values())
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
+            data.set_description(f'loss:{losses}')
+            torch.cuda.empty_cache()
+        torch.save({'model_state_dict':model.state_dict()},f'./models/mask_rcnn_{idx}.pt')
+        print('save checkpoint')
     pass
+
+
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 num_classes = 4
-batch_size = 32
+batch_size = 4
 learning_rate = 1.0e-3
-epoch = 100
+epoch = 30
 mean, std = 0.5557, 0.2051
 hidden_layer = 256
 path = './class_data/Train/*/'
 size = (256,256)
-transform = T.Compose([T.Resize(size),T.Normalize(mean,std)])
+transform = T.Compose([T.Resize(size)])
+kf = KFold(n_splits=4,shuffle=True,random_state=1)
 
 if __name__ == '__main__':
     model = torchvision.models.detection.maskrcnn_resnet50_fpn()
@@ -47,6 +55,14 @@ if __name__ == '__main__':
     model = model.to(device)
     optimizer = torch.optim.AdamW(model.parameters(),learning_rate)
     loss_func = IoULoss()
-    train_set = Image_dataset(path, transform)
-    train_loader = DataLoader(train_set, batch_size, shuffle=True)
-    training(model,train_loader, epoch, loss_func, optimizer, device)
+    train_set = Image_dataset(path, transform=T.Normalize(mean,std))
+
+    
+    for idx, (train_id, valid_id) in enumerate(kf.split(train_set)):
+        train_sample = SubsetRandomSampler(train_id)
+        valid_sample = SubsetRandomSampler(valid_id)
+        train_loader = DataLoader(train_set,batch_size=batch_size,collate_fn=my_collate_fn,sampler=train_sample)
+        valid_loader = DataLoader(train_set,batch_size=batch_size,collate_fn=my_collate_fn,sampler=valid_sample)
+        for e in range(epoch):
+            training(model,train_loader,loss_func,optimizer,device,idx,e)
+            
